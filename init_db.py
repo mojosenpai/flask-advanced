e9fa9d19ec21b5eb54c557954a77b4099e0fc9ed
mojosenpai <mojosamaxiii@gmail.com>
2023-04-26 21:02:01 +0430

    end of session 3

--- init_db.py
+++ init_db.py
@@ -1,4 +1,12 @@
 from app import app, db
+from app import User, Post, Category, post_category
 
 with app.app_context():
     db.create_all()
+
+u1 = User(username='Arash', password='123456', role='author')
+p1 = Post(title='Welcome', desc='Welcome to my weblog', author=u1)
+
+with app.app_context():
+    db.session.add_all([u1, p1])
+    db.session.commit()
