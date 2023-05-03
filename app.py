fa61131b2bba3ce519040e4816171993423c7376
mojosenpai <mojosamaxiii@gmail.com>
2023-05-03 20:15:37 +0430

    added login manager

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask
+from flask import Flask, url_for, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length
+from flask_bcrypt import Bcrypt
 from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisismysecretkey'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
 with app.app_context():
     db = SQLAlchemy(app)
+    bcrypt=Bcrypt(app)
 
 post_category = db.Table('post_category', 
                         db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
@@ -33,4 +44,60 @@ class User(db.Model, UserMixin):
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
-    
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={'placeholder': 'username...'})
+    password = PasswordField(validators=[InputRequired(), Length(min=6, max=20)],
+                           render_kw={'placeholder': 'password...'})
+    submit = SubmitField('Create Account')
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={'placeholder': 'username...'})
+    password = PasswordField(validators=[InputRequired(), Length(min=6, max=20)],
+                           render_kw={'placeholder': 'password...'})
+    submit = SubmitField('Login')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password, role='author')
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/logout', methods=['POST', 'GET'])
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', current_user=current_user)
+
+app.run(debug=True)
